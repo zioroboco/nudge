b10f172410ecb8c346c1aa5a336dc20c230346f3
zioroboco <zioroboco@gmail.com>
2018-05-21 19:44:55 +1000

    Set AWS region according to environment

--- src/secrets.ts
+++ src/secrets.ts
@@ -1,14 +1,22 @@
 import { SSM } from "aws-sdk"
 
-const getSecret = (name: string): Promise<string | void> =>
-  new SSM({ region: process.env.AWS_DEFAULT_REGION })
+const { REGION, AWS_DEFAULT_REGION } = process.env
+
+/**
+ * Retrieve a key at runtime from an encrypted SSM parameter.
+ * @param name The name of (or path to) the SSM parameter.
+ * @param region The region in which the SSM parameter is set.
+ */
+const getSecret = (
+  name: string,
+  region = REGION || AWS_DEFAULT_REGION!
+): Promise<string | void> =>
+  new SSM({ region })
     .getParameter({ Name: name, WithDecryption: true })
     .promise()
     .then(result => {
       if (result.Parameter) {
         return result.Parameter.Value
-      } else {
-        throw new Error(`${name} not defined in SSM parameters`)
       }
     })
 
--- tests/secrets.spec.ts
+++ tests/secrets.spec.ts
@@ -1,7 +1,11 @@
 import { getSecret } from "../src/secrets"
 
+// The AWS region of the testing environment
+const region = process.env.AWS_DEFAULT_REGION!
+
 it("gets the test secret", async () => {
-  process.env.AWS_DEFAULT_REGION = "us-east-1"
-  const secret = await getSecret("TEST_SECRET")
-  expect(secret).toBe("ding")
+  // Test secret set as an encrypted SSM parameter in advance
+  const secret = { name: "TEST_SECRET", value: "ding" }
+  const result = await getSecret(secret.name, region)
+  expect(result).toBe(secret.value)
 })
